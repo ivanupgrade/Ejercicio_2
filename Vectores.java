19170c57a98bbb09eaa52d1c6912e3f9e998964f
Ivan <ivan.hernandez@bootcamp-upgrade.com>
2025-03-13 20:52:35 +0100

    creada clase Vectores.java

--- Vectores.java
+++ Vectores.java
@@ -1,3 +1,9 @@
 public class Vectores {
-    
+    public static void mostrarVector(int[] vector) {
+        System.out.print("valores");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.printf("%d ", vector[i]);
+        }
+        System.out.println();
+    }
 }
