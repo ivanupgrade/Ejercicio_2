9e838b5092ed92723628ecbf2110b4a27fc3ba7f
Ivan <ivan.hernandez@bootcamp-upgrade.com>
2025-03-13 21:01:18 +0100

    creada clase Numeros.java

--- Numeros.java
+++ Numeros.java
@@ -1,3 +1,9 @@
 public class Numeros {
-    
+    public static boolean par(int numero) {
+        if (numero%2==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
